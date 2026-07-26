76155b3f1102aa264e4e655b6f362ec5a93daba7
Caden921 <jjunc0921@icloud.com>
2026-07-26 21:58:11 +0900

    build.js 업데이트

--- js/build.js
+++ js/build.js
@@ -9,7 +9,7 @@ buildButton.addEventListener("click",buildAddon);
 
 async function buildAddon(){
 
-    if(!project.pack.name){
+    if(!project.pack.name.trim()){
 
         alert("Pack Name을 입력해주세요.");
 
@@ -17,16 +17,7 @@ async function buildAddon(){
 
     }
 
-    const addon=new JSZip();
-
-    const bp=addon.folder("behavior_pack");
-    const rp=addon.folder("resource_pack");
-
-    const bpHeader=crypto.randomUUID();
-    const bpModule=crypto.randomUUID();
-
-    const rpHeader=crypto.randomUUID();
-    const rpModule=crypto.randomUUID();
+    const bp=new JSZip();
 
     bp.file("manifest.json",JSON.stringify({
 
@@ -35,7 +26,7 @@ async function buildAddon(){
         header:{
             name:project.pack.name,
             description:project.pack.description,
-            uuid:bpHeader,
+            uuid:crypto.randomUUID(),
             version:[1,0,0],
             min_engine_version:[1,21,0]
         },
@@ -43,18 +34,18 @@ async function buildAddon(){
         modules:[{
 
             type:"script",
-
             language:"javascript",
-
-            uuid:bpModule,
-
+            uuid:crypto.randomUUID(),
             version:[1,0,0],
-
             entry:"scripts/main.js"
 
         }]
 
     },null,4));
+
+    bp.folder("scripts").file("main.js","");
+
+    const rp=new JSZip();
 
     rp.file("manifest.json",JSON.stringify({
 
@@ -63,7 +54,7 @@ async function buildAddon(){
         header:{
             name:project.pack.name+" RP",
             description:project.pack.description,
-            uuid:rpHeader,
+            uuid:crypto.randomUUID(),
             version:[1,0,0],
             min_engine_version:[1,21,0]
         },
@@ -71,30 +62,27 @@ async function buildAddon(){
         modules:[{
 
             type:"resources",
-
-            uuid:rpModule,
-
+            uuid:crypto.randomUUID(),
             version:[1,0,0]
 
         }]
 
     },null,4));
 
-    bp.folder("scripts")
-      .file("main.js","");
+    const bpBlob=await bp.generateAsync({type:"blob"});
+    const rpBlob=await rp.generateAsync({type:"blob"});
 
-    const blob=await addon.generateAsync({
+    const addon=new JSZip();
 
-        type:"blob"
+    addon.file("behavior_pack.mcpack",bpBlob);
+    addon.file("resource_pack.mcpack",rpBlob);
 
-    });
+    const blob=await addon.generateAsync({type:"blob"});
 
     const a=document.createElement("a");
 
     a.href=URL.createObjectURL(blob);
-
     a.download=project.pack.name+".mcaddon";
-
     a.click();
 
     URL.revokeObjectURL(a.href);
